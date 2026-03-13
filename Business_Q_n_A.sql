86e64184984806b984fbed5de3ce3fcb33cb738f
Victor Oduh <victoroduh5@gmail.com>
2026-03-13 16:41:01 +0000

    Question_1

--- Business_Q_n_A.sql
+++ Business_Q_n_A.sql
@@ -1,4 +1,27 @@
 --Question 1: What are the top-paying jobs by roles?
+/*
+-   List the top-10 heighest paying jobs for the role (Data Analyst) that are remote.
+    The output should include the Jop_id, job title, company name, salary, job_location, schedule type and job posted date.
+
+-   Only output jobs with non-null salary values.
+*/
+
+SELECT 
+    job_id,
+    cd.name as company_name,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM job_postings_fact as jpf
+LEFT JOIN company_dim as cd 
+ON jpf.company_id = cd.company_id
+WHERE job_location = 'Anywhere' 
+AND job_title_short = 'Data Analyst'
+AND salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10;
 
 --Question 2: What are the skills required for the top-paying jobs?
 
