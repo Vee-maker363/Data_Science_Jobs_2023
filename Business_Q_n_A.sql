a2a13c9a05988c5e216ddf11ee9aba5a092eea61
Victor Oduh <victoroduh5@gmail.com>
2026-03-13 16:20:10 +0000

    Q and A SQL file created

---
+++ Business_Q_n_A.sql
@@ -0,0 +1,9 @@
+--Question 1: What are the top-paying jobs by roles?
+
+--Question 2: What are the skills required for the top-paying jobs?
+
+--Question 3: What are the most in-demand skills for the top-paying jobs?
+
+--Question 4: What are the skills based on salary for the roles?
+
+--Question 5: What are the most optimal () skills to learn? 
